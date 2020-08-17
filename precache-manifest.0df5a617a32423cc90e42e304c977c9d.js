4e4984dc5938db58a3bbce9e85c8914f1232910e
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-08-17 11:53:06 -0300

    Updates

---
+++ precache-manifest.0df5a617a32423cc90e42e304c977c9d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435531390918f83f753d984012c4e165",
+    "url": "/aig-grupo5/index.html"
+  },
+  {
+    "revision": "054dfb1b4a68bfc0d818",
+    "url": "/aig-grupo5/static/css/main.83d44058.chunk.css"
+  },
+  {
+    "revision": "e6172c570ca53cdf8d97",
+    "url": "/aig-grupo5/static/js/2.26277251.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/aig-grupo5/static/js/2.26277251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "054dfb1b4a68bfc0d818",
+    "url": "/aig-grupo5/static/js/main.028976eb.chunk.js"
+  },
+  {
+    "revision": "04762f98375d1cc3bf7e",
+    "url": "/aig-grupo5/static/js/runtime-main.70779631.js"
+  },
+  {
+    "revision": "c57ede1de60f117e50b6d0cb5e55240c",
+    "url": "/aig-grupo5/static/media/Roboto-LightItalic.c57ede1d.ttf"
+  },
+  {
+    "revision": "7e4af5d0d573f2212a9ae589695d36ff",
+    "url": "/aig-grupo5/static/media/biglogo.7e4af5d0.png"
+  },
+  {
+    "revision": "51615cc1b652851cc46fc347b89b9597",
+    "url": "/aig-grupo5/static/media/bottles.51615cc1.svg"
+  },
+  {
+    "revision": "a240077cc4923fd39bf41d87e7833083",
+    "url": "/aig-grupo5/static/media/cart.a240077c.svg"
+  },
+  {
+    "revision": "3d733ec5feea4581255a7f2ebeff96a3",
+    "url": "/aig-grupo5/static/media/logo.3d733ec5.svg"
+  },
+  {
+    "revision": "b550c33e605234ba9aaea5184961b29f",
+    "url": "/aig-grupo5/static/media/rubish.b550c33e.svg"
+  },
+  {
+    "revision": "a75c4d09a8e5bd9554294c91b17f3ef0",
+    "url": "/aig-grupo5/static/media/tela1R.a75c4d09.png"
+  },
+  {
+    "revision": "204ec3502247909211313c9acfbd8e6d",
+    "url": "/aig-grupo5/static/media/tela2R.204ec350.png"
+  },
+  {
+    "revision": "228cd08f4aab4f79095b20c6acf9e040",
+    "url": "/aig-grupo5/static/media/user.228cd08f.svg"
+  }
+]);
